3ec71b750bbf052b811ad7e0a6905bd23a43ade6
Tyler Nijmeh <tylernij@gmail.com>
2020-06-22 21:34:59 -0700

    sched/sysctl: Further tweak the scheduler
    kernel.sched_child_runs_first: 0 --> 1
    When forking a child process from the parent, execute the child process before the parent process. This usually shaves down some latency on task initializations, since most of the time the child process is doing some form of heavy lifting.
    
    kernel.sched_nr_migrate: 32 --> 128
    When migrating tasks between CPUs, allow the scheduler to migrate twice as many as usual. This should increase scheduling latency marginally, but increase the performance of SCHED_OTHER tasks. In testing, cyclictest reported a 2 microsecond increase in average latency, an a decrease in maximum latency of SCHED_FIFO tasks.
    
    kernel.sched_min_granularity_ns: 2500000 (2.5ms)
    Set the minimum task scheduling period to 2.5ms. With kernel.sched_latency_ns set to 2.5ms, this means that 4 active tasks may execute within the 10ms scheduling period before we exceed it. Originally, this value was set to 1ms. After benchmarking using hackbench -pl 8000, it was determined that a higher value reduces hackbench times significantly. The tradeoff is scheduling latency.
    
    kernel.sched_wakeup_granularity_ns: 1000000 (1ms) --> 10000000 (10ms)
    Require the current task to be surpassing the new task in vmruntime by 10ms instead of 1ms before preemption occurs. In testing, hackbench -pl 8000 times were reduced by ~94% (NOT a typo).
    
    kernel.sched_min_task_util_for_boost: 35
    This value effects if tasks should be migrated to a higher performant CPU if it's utilization is above this amount (during sched_boost). Allow tasks to be migrated upwards if the user is triggering a touch boost and the task is above 35% utilization.
    
    kernel.sched_min_task_util_for_colocation: 25
    This value is the same as the former, except it occurs only for top-app tasks (which are of greater priority than background tasks). Lower this value a bit to use big clusters more for top-app tasks.
    
    [ghostrider-reborn] taken from tytydraco:
    
    Change-Id: Id34eb5e8eb379e747e924111bfe78a856a6990f6
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Shashank Baghel <theradcolor@gmail.com>
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -522,7 +522,7 @@ static struct ctl_table kern_table[] = {
 		.procname	= "sched_child_runs_first",
 		.data		= &sysctl_sched_child_runs_first,
 		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
+		.mode		= 0444,
 		.proc_handler	= proc_dointvec,
 	},
 #if defined(CONFIG_PREEMPT_TRACER) || defined(CONFIG_IRQSOFF_TRACER)
@@ -617,7 +617,7 @@ static struct ctl_table kern_table[] = {
 		.procname	= "sched_wakeup_granularity_ns",
 		.data		= &sysctl_sched_wakeup_granularity,
 		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
+		.mode		= 0444,
 		.proc_handler	= sched_proc_update_handler,
 		.extra1		= &min_wakeup_granularity_ns,
 		.extra2		= &max_wakeup_granularity_ns,
@@ -643,7 +643,7 @@ static struct ctl_table kern_table[] = {
 		.procname	= "sched_nr_migrate",
 		.data		= &sysctl_sched_nr_migrate,
 		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
+		.mode		= 0444,
 		.proc_handler	= proc_dointvec,
 	},
 	{
