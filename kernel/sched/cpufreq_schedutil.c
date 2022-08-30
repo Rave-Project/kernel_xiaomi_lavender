c44784ef4b4b680ee38ec01f35fe1e7f74dc04a8
Peppe289 <gsperanza204@gmail.com>
2022-08-30 11:35:43 +0200

    schedutil: remove unlikely when check for next update
    We are managing a change in frequency. Remove the forecast
    
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -89,7 +89,7 @@ static bool sugov_should_update_freq(struct sugov_policy *sg_policy, u64 time)
 {
 	s64 delta_ns;
 
-	if (unlikely(sg_policy->need_freq_update)) {
+	if (sg_policy->need_freq_update) {
 		return true;
 	}
 
